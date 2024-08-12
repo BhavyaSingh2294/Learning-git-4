7188b78e2a5f37eef8f99526a98915c87a0fd782
Bhavya_Singh <bhavyasingh2294@gmail.com>
2024-08-12 22:17:26 +0530

    5th Commit

--- Main.java
+++ Main.java
@@ -1,21 +1,21 @@
-import java.util.*;
-public class Main {
+// import java.util.*;
+// public class Main {
 	
-	public static void main(String[] args) {
-		// Write your code here
-        Scanner sc = new Scanner(System.in);
-        int y = sc.nextInt(); 
+// 	public static void main(String[] args) {
+// 		// Write your code here
+//         Scanner sc = new Scanner(System.in);
+//         int y = sc.nextInt(); 
 
-        int count = 0;
-        int n = 0;
+//         int count = 0;
+//         int n = 0;
 
-        while (count < y) {
-            int term = 3 * n + 2;
-            if (term % 4 != 0) {
-                System.out.print(term + " ");
-                count++;
-            }
-            n++;
-        }
-	}
-}
+//         while (count < y) {
+//             int term = 3 * n + 2;
+//             if (term % 4 != 0) {
+//                 System.out.print(term + " ");
+//                 count++;
+//             }
+//             n++;
+//         }
+// 	}
+// }
--- python.py
+++ python.py
@@ -1,19 +1,19 @@
-def function1(number):
-   list1 = []
-   for i in range(1, number):
-      if number % i == 0: 
-        list1.append(i)
-   return list1
+# def function1(number):
+#    list1 = []
+#    for i in range(1, number):
+#       if number % i == 0: 
+#         list1.append(i)
+#    return list1
    
-def function2(n1,n2):
- flag = False
- list2 = []
-for i in function1 (n1):
-      for j in function1(n2):
-         if i == j:
-          flag = True
-          list2.append(i)
-if len(list2) > 0:
-    print("Completed")
+# def function2(n1,n2):
+#  flag = False
+#  list2 = []
+# for i in function1 (n1):
+#       for j in function1(n2):
+#          if i == j:
+#           flag = True
+#           list2.append(i)
+# if len(list2) > 0:
+#     print("Completed")
 
-function(0,2)
+# function(0,2)
